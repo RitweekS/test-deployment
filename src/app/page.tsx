cf0b483d0667e7c4d3e3f3b015e055d9fb6c4b42
Ritweek <ritweeks77@gmail.com>
2024-10-12 17:23:32 +0530

    remove hyper link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,21 +22,6 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
